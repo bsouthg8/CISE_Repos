2948f8de2a707235dbdff2fb484c44b59c007bf2
bsouthg8 <bsouthg8@gmail.com>
2024-08-16 16:14:06 +1200

    style: text change to better suit context

--- worksheet1/cise-react-learn/src/App.js
+++ worksheet1/cise-react-learn/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Agile is about Values and Principles
         </a>
       </header>
     </div>
